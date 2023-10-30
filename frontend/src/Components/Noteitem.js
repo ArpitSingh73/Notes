42b7b9ece9639e9d6dbaaf80b066a841f51888e8
ArpitSingh73 <arpitsingh73073@gmail.com>
2023-10-30 23:55:39 +0530

    bug fixed

--- frontend/src/Components/Noteitem.js
+++ frontend/src/Components/Noteitem.js
@@ -6,12 +6,38 @@ const Noteitem = (props) => {
   const { deleteNote } = context;
   const { note, updateNote } = props;
   return (
-    <div    style={{width : "70%"   , marginLeft :"15%"   }}>
-      <div className="card my-3" style={{ border:"solid 3px 	#778899", boxShadow :"3px 3px 2px",backgroundColor:"#d9d9d9"}} >
+    <div style={{ width: "70%", marginLeft: "15%" }}>
+      <div
+        className="card my-3"
+        style={{
+          border: "solid 3px 	#778899",
+          boxShadow: "3px 3px 2px",
+          backgroundColor: "#d9d9d9",
+        }}
+      >
         <div className="card-body">
           <div className="d-flex align-items-center">
-            <h5 className="card-title">{note.title}   |</h5>
-            <i
+            <h5 className="card-title">{note.title} |</h5>
+
+            <button
+              className="btn btn-danger mx-2"
+              onClick={() => {
+                deleteNote(note._id);
+                props.show("notes deleted", "success");
+              }}
+            >
+              Delete{" "}
+            </button>
+            <button
+              className="btn btn-success"
+              onClick={() => {
+                updateNote(note);
+              }}
+            >
+              Edit{" "}
+            </button>
+
+            {/* <i
               className="fa-solid fa-trash mx-4"
               
               onClick={() => {
@@ -26,7 +52,7 @@ const Noteitem = (props) => {
               onClick={() => {
                 updateNote(note);
               }}
-            ></i>
+            ></i> */}
           </div>
           <p className="card-text">{note.description}</p>
         </div>
